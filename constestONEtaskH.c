804a27dd2797f5bae7ad9473c62315e5266b4340
shpvckdotov <126060577+shpvckdotov@users.noreply.github.com>
2023-05-11 23:06:00 +0300

    fixed H

--- constestONEtaskH.c
+++ constestONEtaskH.c
@@ -8,11 +8,7 @@
 #define _FIRST_NUMERICAL_SEQUENCE_CONST_ 123
 #define _SECOND_NUMERICAL_SEQUENCE_CONST_ 45
 #define _MEDIAN_PARTITION_CONST_ 5
-
-
-
-
-
+#define _MID_DELIMETER_CONST 2
 
 struct _Container {
 	int* lower;
@@ -20,24 +16,17 @@ struct _Container {
 	int* equals;
 };
 
-
 typedef struct _Container Container;
-
-
-
-
-
 
 int* allocateArray(int size) {
 	return (int*)malloc(size * sizeof(int));
 }
 
-void copyArray(int* to, int* from, int size) {
-	for (int i = 0; i < size; i++) {
+void copyArray(int* to, int* from, const size_t size) {
+	for (size_t i = 0; i < size; i++) {
 		to[i] = from[i];
 	}
 }
-
 
 void swap(int* first, int* second) {
 	int tmp = *second;
@@ -45,8 +34,7 @@ void swap(int* first, int* second) {
 	*first = tmp;
 }
 
-
-void partition(Container* container, int* a, int size, int* sizeLower, int* sizeEquals, int pivot) {
+void partition(Container* container, int* a, const size_t size, int* sizeLower, int* sizeEquals, int pivot) {
 	int counterLower = 0;
 	int counterEquals = 0;
 	int counterBigger = 0;
@@ -64,29 +52,25 @@ void partition(Container* container, int* a, int size, int* sizeLower, int* size
 	container->lower = allocateArray(counterLower);
 	container->bigger = allocateArray(counterBigger);
 	container->equals = allocateArray(counterEquals);
-	int counterl = 0;
-	int countere = 0;
-	int counterb = 0;
+	counterLower = 0;
+	counterEquals = 0;
+	counterBigger = 0;
 	for (int i = 0; i < size; i++) {
 		if (a[i] < pivot) {
-			container->lower[counterl++] = a[i];
+			container->lower[counterLower++] = a[i];
 		}
 		if (a[i] > pivot) {
-			container->bigger[counterb++] = a[i];
+			container->bigger[counterBigger++] = a[i];
 		}
 		if (a[i] == pivot) {
-			container->equals[countere++] = a[i];
+			container->equals[counterEquals++] = a[i];
 		}
 	}
-	*sizeLower = counterl;
-	*sizeEquals = countere;
+	*sizeLower = counterLower;
+	*sizeEquals = counterEquals;
 }
 
-
-
-
-
-void selectSort(int* vector, int size) {
+void selectSort(int* vector, const size_t size) {
 	for (int i = 0; i < size; i++) {
 		int min = i;
 		for (int j = i; j < size; j++) {
@@ -98,77 +82,63 @@ void selectSort(int* vector, int size) {
 	}
 }
 
-int kth(int* a, int size, int k) {
+void tempArrayInit(int* ar,int temp,int* j) {
+	size_t counter = 0;
+	while (counter < temp) {
+		ar[counter++] = a[*j];
+		*j++;
+	}
+}
+
+void freeArrays(int* first, int* second, int* third) {
+	free(first);
+	free(second);
+	free(third);
+}
+
+int kth(int* a, const size_t size, int k) {
+	int ar[_MEDIAN_PARTITION_CONST_];
 	if (size < _MEDIAN_PARTITION_CONST_) {
 		int temp = size % _MEDIAN_PARTITION_CONST_;
-		int ar[_MEDIAN_PARTITION_CONST_];
-		int counter = 0;
-		while (counter < temp) {
-			ar[counter] = a[counter];
-			counter++;
-		}
+		size_t counter;
+		tempArrayInit(ar, temp, &counter);
 		selectSort(ar, temp);
 		return ar[k];
 	}
-	int count = (size) / _MEDIAN_PARTITION_CONST_;
-	int j = 0;
+	size_t count = (size) / _MEDIAN_PARTITION_CONST_;
+	size_t j = 0;
 	int* medians = allocateArray(count + 1);
-	int i = 0;
+	size_t i = 0;
 	for (i = 0; i < count; i++) {
-		int ar[_MEDIAN_PARTITION_CONST_];
-		int counter = 0;
-		while (counter < 5) {
-			ar[counter++] = a[j];
-			j++;
-		}
+		tempArrayInit(ar, _MEDIAN_PARTITION_CONST_, &j);
 		selectSort(ar, _MEDIAN_PARTITION_CONST_);
-		medians[i] = ar[2];
+		medians[i] = ar[_MID_DELIMETER_CONST];
 	}
 	int temp = (size) % _MEDIAN_PARTITION_CONST_;
-	int ar[_MEDIAN_PARTITION_CONST_];
-	int counter = 0;
-	while (counter < temp) {
-		ar[counter++] = a[j];
-		j++;
-	}
+	tempArrayInit(ar, temp, &j);
 	selectSort(ar, temp);
 	if (temp != 0) {
-		medians[i] = ar[temp / 2];
+		medians[i] = ar[temp / _MID_DELIMETER_CONST];
 		count++;
 	}
-	int m = kth(medians, count, count / 2);
-	int lowerSize;
-	int equalsSize;
+	int m = kth(medians, count, count / _MID_DELIMETER_CONST);
+	size_t lowerSize;
+	size_t equalsSize;
 	Container container;
 	partition(&container, a, size, &lowerSize, &equalsSize, m);
 	if (k < lowerSize) {
-		free(container.equals);
-		free(container.bigger);
-		free(a);
+		freeArrays(container.bigger, container.equals, a);
 		return kth(container.lower, lowerSize, k);
 	}
 	if (k < lowerSize + equalsSize) {
-		free(container.lower);
-		free(container.equals);
-		free(container.bigger);
+		freeArrays(container.bigger, container.equals,container.lower);
 		free(a);
 		return m;
 	}
-	free(container.lower);
-	free(container.equals);
-	free(a);
+	freeArrays(container.lower, container.equals, a);
 	return kth(container.bigger, size - equalsSize - lowerSize, k - equalsSize - lowerSize);
 }
-
-
-int main() {
-	int n;
-	int k;
-	int A0;
-	int A1;
-	if (scanf("%d%d%d%d", &n, &k, &A0, &A1) == 1) {
-
-	}
+void initArray(int* array,int A0,int A1) {
 	int* array = allocateArray(n);
 	array[0] = A0;
 	array[1] = A1;
@@ -176,6 +146,14 @@ int main() {
 		int a = (array[i - 1] * _FIRST_NUMERICAL_SEQUENCE_CONST_ + array[i - 2] * _SECOND_NUMERICAL_SEQUENCE_CONST_) % (_MODCONST_);
 		array[i] = a;
 	}
+}
+int main() {
+	int n,k,A0,A1;
+	if (scanf("%d%d%d%d", &n, &k, &A0, &A1) == 1) {
+
+	}
+	int* array = NULL;
+	initArray(array, A0, A1);
 	k--;
 	int a = kth(array, n, k);
 	printf("%d", a);
