62a8ecf3413f8963fa2687029bc3e05d671df8c7
shpvckdotov <126060577+shpvckdotov@users.noreply.github.com>
2023-12-23 01:42:18 +0300

    updated e

--- contest2taskE.cpp
+++ contest2taskE.cpp
@@ -4,32 +4,54 @@
 #include <string>
 #include <set>
 
-#define SPACE ' '
-#define NEW_LINE '\n'
-
 size_t timer = 0;
 size_t INF = 1e9;
 
-void dfs(const std::vector<std::vector<std::pair<size_t, size_t>>>& graph, std::vector<bool>& used, std::vector<size_t>& ret,
-	std::vector<size_t>& tin, std::vector<size_t>& tout, std::vector<size_t>& parents, std::set<size_t>& bridges, size_t s, size_t parent = -1) {
+void dfs(const std::vector<std::vector<std::pair<size_t, size_t>>>& graph, std::vector<bool>& used_vertexes, std::vector<size_t>& tree_return_time,
+	std::vector<size_t>& vertex_enter_time, std::vector<size_t>& vertex_time_out, std::vector<size_t>& parents, std::set<size_t>& bridges, size_t s, size_t parent = -1) {
 	parents[s] = parent;
-	tin[s] = timer++;
-	ret[s] = tin[s];
-	used[s] = true;
+	vertex_enter_time[s] = timer++;
+	tree_return_time[s] = vertex_enter_time[s];
+	used_vertexes[s] = true;
 	for (auto to : graph[s]) {
 		if (to.first == parent) {
 			continue;
 		}
-		if (used[to.first]) {
-			ret[s] = std::min(ret[s], tin[to.first]);
+		if (used_vertexes[to.first]) {
+			tree_return_time[s] = std::min(tree_return_time[s], vertex_enter_time[to.first]);
 		}
 		else {
-			dfs(graph, used, ret, tin, tout, parents, bridges, to.first, s);
-			ret[s] = std::min(ret[s], ret[to.first]);
-			if (ret[to.first] == tin[to.first]) {
+			dfs(graph, used_vertexes, tree_return_time, vertex_enter_time, vertex_time_out, parents, bridges, to.first, s);
+			tree_return_time[s] = std::min(tree_return_time[s], tree_return_time[to.first]);
+			if (tree_return_time[to.first] == vertex_enter_time[to.first]) {
 				bridges.insert(to.second);
 			}
 		}
+	}
+}
+
+void graph_initiation(std::vector<std::vector<std::pair<size_t, size_t>>>& graph) {
+	for (size_t i = 1; i <= m; i++) {
+		size_t first, second;
+		std::cin >> first >> second;
+		graph[first].push_back(std::make_pair(second, i));
+		graph[second].push_back(std::make_pair(first, i));
+	}
+}
+
+void answer_building(const std::vector<std::vector<std::pair<size_t, size_t>>>& graph, std::vector<bool>& used_vertexes, std::vector<size_t>& tree_return_time,
+	std::vector<size_t>& vertex_enter_time, std::vector<size_t>& vertex_time_out, std::vector<size_t>& parents, std::set<size_t>& bridges, size_t s, size_t parent = -1) {
+	for (size_t vertex = 1; vertex <= n; vertex++) {
+		if (!used_vertexes[vertex]) {
+			dfs(graph, used_vertexes, tree_return_time, vertex_enter_time, vertex_time_out, parents, bridges, i);
+		}
+	}
+}
+
+void answer_printing(const std::set<size_t>& bridges;) {
+	std::cout << bridges.size() << '\n';
+	for (auto bridge : bridges) {
+		std::cout << bridge << ' ';
 	}
 }
 
@@ -38,26 +60,15 @@ int main() {
 	std::cin.tie(0);
 	size_t n, m;
 	std::cin >> n >> m;
-	std::vector<bool> used(n + 1, false);
+	std::vector<bool> used_vertexes(n + 1, false);
 	std::vector<std::vector<std::pair<size_t, size_t>>> graph(n + 1);
-	std::vector<size_t> tin(n + 1, 0);
-	std::vector<size_t> tout(n + 1, 0);
-	std::vector<size_t> ret(n + 1, INF);
-	std::vector<size_t> parents(n + 1, 0);
+	std::vector<size_t> vertex_enter_time(n + 1, 0), vertex_time_out(n + 1, 0), tree_return_time(n + 1, INF), std::vector<size_t> parents(n + 1, 0);
 	std::set<size_t> bridges;
-	for (size_t i = 1; i <= m; i++) {
-		size_t first, second;
-		std::cin >> first >> second;
-		graph[first].push_back(std::make_pair(second, i));
-		graph[second].push_back(std::make_pair(first, i));
-	}
+	graph_initiation(graph);
 	for (size_t i = 1; i <= n; i++) {
-		if (!used[i]) {
-			dfs(graph, used, ret, tin, tout, parents, bridges, i);
+		if (!used_vertexes[i]) {
+			dfs(graph, used_vertexes, tree_return_time, vertex_enter_time, vertex_time_out, parents, bridges, i);
 		}
 	}
-	std::cout << bridges.size() << NEW_LINE;
-	for (auto bridge : bridges) {
-		std::cout << bridge << SPACE;
-	}
+	answer_printing(bridges);
 }
