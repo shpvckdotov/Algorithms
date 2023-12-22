4ef156c13b7a0edfe6f89e5d493a76de53402710
shpvckdotov <126060577+shpvckdotov@users.noreply.github.com>
2023-12-23 02:17:14 +0300

    updated

--- contest1taskE.cpp
+++ contest1taskE.cpp
@@ -6,7 +6,7 @@
 #define INF 1e9
 #define BASE_FOR_PAIR { -1, -1 }
 
-void min_sum(std::vector<std::vector<std::pair<int, int>>>& pred, std::vector<std::vector<int>>& dp,
+void min_sum(std::vector<std::vector<std::pair<int, int>>>& previous, std::vector<std::vector<int>>& distances,
     std::vector<std::vector<int>> mid_distances, const std::vector<int>& houses, int n, int m){
     for (int l = 1; l <= n; l++) {
         for (int r = l; r <= n; r++) {
@@ -16,53 +16,60 @@ void min_sum(std::vector<std::vector<std::pair<int, int>>>& pred, std::vector<st
             }
         }
     }
-    dp[0][0] = 0;
+    distances[0][0] = 0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j <= n; j++) {
             for (int r = j + 1; r <= n; r++) {
-                if (dp[i + 1][r] > dp[i][j] + mid_distances[j + 1][r]) {
-                    dp[i + 1][r] = dp[i][j] + mid_distances[j + 1][r];
-                    pred[i + 1][r] = { j, r };
+                if (distances[i + 1][r] > distances[i][j] + mid_distances[j + 1][r]) {
+                    distances[i + 1][r] = distances[i][j] + mid_distances[j + 1][r];
+                    previous[i + 1][r] = { j, r };
                 }
             }
         }
     }
 }
 
-void answer_building(const std::vector<std::vector<std::pair<int, int>>>& pred,const std::vector<int>& houses, std::vector<int>& ans, int n, int m) {
-    int i = m;
-    int j = n;
+void answer_building(const std::vector<std::vector<std::pair<int, int>>>& previous, const std::vector<int>& houses, std::vector<int>& influence_points, int n, int m) {
+    int influence_points_iteration = m;
+    int houses_iteration = n;
     while (i != 0) {
-        int l = pred[i][j].first;
-        int r = pred[i][j].second;
-        int u = (l + r) / 2;
-        if ((l + r) % 2) {
-            u++;
+        int left = previous[i][j].first;
+        int right = previous[i][j].second;
+        int mid = (left + right) / 2;
+        if ((left + right) % 2) {
+            mid++;
         }
-        ans.push_back(houses[u]);
-        j = l;
-        i--;
+        influence_points.push_back(houses[u]);
+        houses_iteration = l;
+        influence_points_iteration--;
     }
-    reverse(ans.begin(), ans.end());
+    reverse(influence_points.begin(), influence_points.end());
+}
+
+void houses_initiation(std::vector<int>& houses) {
+    for (int i = 0; i < n; i++) {
+        int house;
+        std::cin >> house;
+        houses.push_back(house);
+    }
+}
+
+void answer_printing(const std::vector<std::vector<int>>& distances, const std::vector<int>& influence_points) {
+    std::cout << distances[m][n] << '\n';
+    for (auto houses : influence_points) {
+        std::cout << houses << ' ';
+    }
 }
 
 int main(){
     int n, m;
     std::cin >> n >> m;
-    std::vector<std::vector<std::pair<int, int>>> pred(m + 1, std::vector<std::pair<int, int>>(n + 1, BASE_FOR_PAIR));
+    std::vector<std::vector<std::pair<int, int>>> previous(m + 1, std::vector<std::pair<int, int>>(n + 1, BASE_FOR_PAIR));
     std::vector<std::vector<int>> mid_distances(n + 1, std::vector<int>(n + 1));
-    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, INF));
-    std::vector<int> houses(1);
-    std::vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        int x;
-        std::cin >> x;
-        houses.push_back(x);
-    }
-    min_sum(pred, dp, mid_distances, houses, n, m);
-    std::cout << dp[m][n] << '\n';
-    answer_building(pred,houses,ans,n,m);
-    for (auto houses : ans) {
-        std::cout << houses << ' ';
-    }
+    std::vector<std::vector<int>> distances(m + 1, std::vector<int>(n + 1, INF));
+    std::vector<int> houses(1), influence_points;
+    houses_initiation(houeses);
+    min_sum(previous, distances, mid_distances, houses, n, m);
+    answer_building(previous, houses, influence_points, n, m);
+    answer_printing(distances, influence_points);
 }
